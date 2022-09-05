454f5a63601ccde462949931ea090b47567556eb
basakk123 <basakk@naver.com>
2022-09-05 11:11:35 +0900

    글쓰기 완료

--- src/main/java/site/metacoding/red/web/BoardsController.java
+++ src/main/java/site/metacoding/red/web/BoardsController.java
@@ -8,35 +8,50 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.RequiredArgsConstructor;
+import oracle.jdbc.proxy.annotation.Post;
+import site.metacoding.red.domain.boards.Boards;
+import site.metacoding.red.domain.boards.BoardsDao;
 import site.metacoding.red.domain.users.Users;
+import site.metacoding.red.web.dto.request.boards.WriteDto;
 
 @RequiredArgsConstructor
 @Controller
 public class BoardsController {
-	
+
 	private final HttpSession session;
+	private final BoardsDao boardsDao;
 
 	// @PostMapping("/boards/{id}/delete")
 	// @PostMapping("/boards/{id}/update")
-	
-	@GetMapping({"/","/boards"})
+
+	@PostMapping("/boards")
+	public String writeBoards(WriteDto writeDto) {
+		Users principal = (Users) session.getAttribute("principal");
+
+		if (principal == null) { // 인증코드
+			return "redirect:/loginForm";
+		}
+
+		boardsDao.insert(writeDto.toEntity(principal.getId()));
+		return "redirect:/";
+	}
+
+	@GetMapping({ "/", "/boards" })
 	public String getBoardList() {
 		return "boards/main";
 	}
-	
+
 	@GetMapping("/boards/{id}")
 	public String getBoardList(@PathVariable Integer id) {
 		return "boards/detail";
 	}
-	
+
 	@GetMapping("/boards/writeForm")
 	public String writeForm() {
-		Users principal = (Users)session.getAttribute("principal"); // 인증 검사, 권한은 필요없음
-		if(principal==null) {
+		Users principal = (Users) session.getAttribute("principal"); // 인증 검사, 권한은 필요없음
+		if (principal == null) { // 인증코드
 			return "redirect:/loginForm";
-		}else {
-			return "boards/writeForm";
 		}
-		
+		return "boards/writeForm"; // 부가코드
 	}
 }
