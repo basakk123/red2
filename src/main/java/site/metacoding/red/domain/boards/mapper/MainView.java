d88a441ac30dbfd8d8c6d95cf6d1848627de90ae
basakk123 <basakk@naver.com>
2022-09-05 11:52:02 +0900

    게시글 목록보기 완료

---
+++ src/main/java/site/metacoding/red/domain/boards/mapper/MainView.java
@@ -0,0 +1,13 @@
+package site.metacoding.red.domain.boards.mapper;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MainView {
+	private Integer id;
+	private String title;
+	private String username;
+
+}
