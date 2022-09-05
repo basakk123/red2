a688ba21f69d13d9cd104507a7e176187e4eb478
basakk123 <basakk@naver.com>
2022-09-05 09:52:10 +0900

    글쓰기 인증 체크완료

--- src/main/java/site/metacoding/red/web/BoardsController.java
+++ src/main/java/site/metacoding/red/web/BoardsController.java
@@ -1,12 +1,20 @@
 package site.metacoding.red.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import lombok.RequiredArgsConstructor;
+import site.metacoding.red.domain.users.Users;
+
+@RequiredArgsConstructor
 @Controller
 public class BoardsController {
+	
+	private final HttpSession session;
 
 	// @PostMapping("/boards/{id}/delete")
 	// @PostMapping("/boards/{id}/update")
@@ -23,6 +31,12 @@ public class BoardsController {
 	
 	@GetMapping("/boards/writeForm")
 	public String writeForm() {
-		return "boards/writeForm";
+		Users principal = (Users)session.getAttribute("principal"); // 인증 검사, 권한은 필요없음
+		if(principal==null) {
+			return "redirect:/loginForm";
+		}else {
+			return "boards/writeForm";
+		}
+		
 	}
 }
